6b0c06bd6d5767417e2a92fa0c713e09465d4e24
Batuhan <91565731+BizonTR@users.noreply.github.com>
2024-08-28 17:43:57 +0300

    auto log out

--- client/main.js
+++ client/main.js
@@ -5,3 +5,31 @@ import 'meteor/aldeed:collection2/static';
 
 import '../client/routes.js'
 import './main.html';
+
+// client/main.js
+if (Meteor.isClient) {
+    let inactivityTimer;
+
+    const handleInactivity = () => {
+        if (Meteor.userId()) {
+            console.log('Kullanıcı etkileşimde bulunmuyor. Oturum kapatılıyor...');
+            Meteor.logout(); // Oturum kapatılıyor
+        }
+    };
+
+    const resetTimer = () => {
+        if (Meteor.userId()) { // Oturum açık mı kontrolü
+            console.log('Kullanıcı etkileşimde bulundu. Timer sıfırlandı.');
+            clearTimeout(inactivityTimer);
+            inactivityTimer = setTimeout(handleInactivity, 20000); // 20 saniye
+        }
+    };
+
+    // Sayfa üzerinde etkileşim tespit etme
+    window.addEventListener('mousemove', resetTimer);
+    window.addEventListener('keydown', resetTimer);
+    window.addEventListener('click', resetTimer);
+
+    // İlk başta timer'ı başlat
+    resetTimer();
+}
